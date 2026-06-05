8037defe7a8276aab0e10d8309a46747efc3e202
DiegoRamos04 <diego2007.ramos@gmail.com>
2026-06-05 16:19:41 -0300

    Fix greeting message formatting in login function

--- src/login.py
+++ src/login.py
@@ -2,7 +2,7 @@ def login():
     while True:
 
         insertname = input("insira seu nome para acessar o sistema:\n")
-        print(f"seja bem vindo {insertname}")
+        print(f"seja bem-vindo, {insertname}")
 
         password = input(
             "para acessar o sistema operacional Aurora insira a senha de acesso:"
